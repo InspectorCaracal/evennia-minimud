faa4aefb44ec02230172022365db7aff1968bf35
InspectorCaracal <51038201+InspectorCaracal@users.noreply.github.com>
2024-06-28 21:05:06 -0600

    actually grant experience on defeat

--- typeclasses/scripts.py
+++ typeclasses/scripts.py
@@ -128,9 +128,8 @@ class CombatScript(Script):
         # grant exp to the other team, if relevant
         if exp := combatant.db.exp_reward:
             for obj in self.db.teams[team - 1]:
-                if obj.db.exp:
-                    obj.msg(f"You gain {exp} experience.")
-                    obj.db.exp += exp
+                obj.msg(f"You gain {exp} experience.")
+                obj.db.exp = (obj.db.exp or 0) + exp
         self.check_victory()
         # remove their combat target if they have one
         del combatant.db.combat_target
